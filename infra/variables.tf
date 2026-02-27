c8cfb4288963064ee94120db2b8d4420050cd411
sudo-suadKF <suadfehric@outlook.com>
2026-02-27 14:09:35 +0100

    change: updated domain name

--- infra/variables.tf
+++ infra/variables.tf
@@ -401,7 +401,7 @@ variable "tg-attach-port" {
 variable "domain-name" {
   description = "Contains the domain name"
   type        = string
-  default     = "amaris.sudosuad.co.uk"
+  default     = "segula.sudosuad.co.uk"
 }
 
 variable "validation-method" {
