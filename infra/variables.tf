1a52bc29fa5f00cd2ebf1a3a3c9694e0d58a39b2
sudo-suadKF <suadfehric@outlook.com>
2026-03-16 14:35:52 +0100

    change: domain name and endpoints

--- infra/variables.tf
+++ infra/variables.tf
@@ -401,7 +401,7 @@ variable "tg-attach-port" {
 variable "domain-name" {
   description = "Contains the domain name"
   type        = string
-  default     = "cgi.sudosuad.co.uk"
+  default     = "opera.sudosuad.co.uk"
 }
 
 variable "validation-method" {
